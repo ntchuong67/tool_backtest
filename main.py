ba783e7e8c8603f58f14ae170cc383f7b809e491
Trung Tran <trung.tranviet94@gmail.com>
2023-03-18 23:50:19 +0700

    update

--- main.py
+++ main.py
@@ -23,5 +23,13 @@ with col1:
 with col2:
     end_date = st.text_input("End Date, e.g. 2023-02-01") # it defaults to current date
 
-print("AAAA")
+try:
+    loader = DataLoader(tickers, start_date ,end_date, minimal=False, data_source = "vnd")
+    data= loader.download()
+    data=data.stack()
+    data=data.reset_index()  
+    st.write(start_date)
+    st.write(end_date)
 
+except Exception as e:
+    st.write(e)
