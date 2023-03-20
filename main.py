54b10ccf383957d848d2ac8dd828654ca9f3cf3d
Trung Tran <trung.tranviet94@gmail.com>
2023-03-20 12:07:43 +0700

    update

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ import plotly.express as px
 i = 1
 state = False
 
-st.set_page_config(page_title = "Stock Portfolio Optimizer - developed by Nguyen Tien Chuong", layout = "wide")
-st.header("Stock Portfolio Optimizer - developed by Nguyen Tien Chuong")
+st.set_page_config(page_title = "QM Capital LLC", layout = "wide")
+st.header("Stock Portfolio Optimizer")
 
 col0, col1, col2 = st.columns(3)
 with col0:
@@ -23,7 +23,7 @@ with col0:
 with col1:
     trial_date = st.text_input("End Traning Date, e.g. 2022-01-01")
 with col2:
-    end_date = st.text_input("End Date, e.g. 2023-02-01") # it defaults to current date
+    end_date = st.text_input("End Date, e.g. 2023-02-01")
 
 col10, col11 = st.columns(2)
 with col10:
@@ -32,7 +32,7 @@ with col11:
     but_01 = st.button('Say hello')
 
 try:
-    if but_01:
+    if but_01:st.button('Say hello')
         state = not state
 
     if state:
