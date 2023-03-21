2bf19a9e5b53c4e18cd2d051d213af2c78471347
Trung Tran <trung.tranviet94@gmail.com>
2023-03-21 22:50:33 +0700

    update

--- main.py
+++ main.py
@@ -41,8 +41,18 @@ if not os.path.isdir("/tmp/ta-lib"):
     # back to the cwd
     os.chdir(default_cwd)
     sys.stdout.flush()
-import talib
 
+from ctypes import *
+
+lib = CDLL("/home/appuser/lib/libta_lib.so.0.0.0")
+# import library
+try:
+    import talib
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--global-option=build_ext", "--global-option=-L/home/appuser/lib/", "--global-option=-I/home/appuser/include/", "ta-lib"])
+finally:
+    import talib
+    
 pd.set_option('expand_frame_repr', False)
 
 STATE = False 
