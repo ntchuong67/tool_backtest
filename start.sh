ecd1c392ba57d6955462e7f2af93f0bb77ac7de5
Ubuntu <ubuntu@ip-172-31-12-88.ap-northeast-1.compute.internal>
2023-03-22 17:45:51 +0000

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+export STREAMLIT_SERVER_PORT=80
+export STREAMLIT_SERVER_PORT=8080
+export PATH="/home/ubuntu/miniconda3/bin:$PATH"
 eval "$(conda shell.bash hook)"
 conda activate stl
 
